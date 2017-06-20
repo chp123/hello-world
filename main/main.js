fc94eb4d753574e37fb397e124df954f18855e55
chp123 <369771187@qq.com>
2017-06-20 10:52:59 +0800

    question3

--- main/main.js
+++ main/main.js
@@ -2,7 +2,14 @@
     console.log("Debug Info");
     return 'Hello World!';
 };*/
-module.exports = function main(a,b) {
+/*module.exports = function main(a,b) {
     console.log("Debug Info");
     return a%b;
+};*/
+module.exports = function main(a) {
+	var sum=0;
+    for(var i=0;i<a.length;i++){
+    	sum+=parseInt(a[i]);
+    }
+    return sum;
 };
